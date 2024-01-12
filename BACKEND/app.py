0de5c1b957e165ddac7f72dbefe04dfaa485c0f6
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2024-01-11 23:34:05 -0300

    pepe

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -6,12 +6,6 @@ def raiz():
     titulo = "pagina inicio"
     return '<h5 class="card-title">Bienvenido a nosotros</h5>'
 
-# ruta para nosotros
-@app.route('/nosotros')
-def nosotros():
-    titulo = "nosotros"
-    return render_template('nosotros.html', titulo=titulo)
-
 # bloque de prueba
 if __name__ == "__main__":
     app.run(debug=True)
