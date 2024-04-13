59d700f4150b8b6989ca933591e3cb911a19a75d
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2024-04-13 18:37:08 -0300

    arrays

--- BACKEND/libs/login_email.py
+++ BACKEND/libs/login_email.py
@@ -13,9 +13,9 @@ def login_email_api(email,password):
             data = []
 
             for id,username,password,email,token_header,token_acceso,rol,balance,a2f in resp_1:
-                data.append({"id":f"{id}","username":f"{username}","email":f"{email}","token_header":f"{token_header}","token_acceso":f"{token_acceso}","rol":f"{rol}","balance":f"{balance}","a2f":f"{a2f}"})
+                data.append({"id":id,"username":username,"email":email,"token_header":token_header,"token_acceso":token_acceso,"rol":rol,"balance":balance,"a2f":a2f})
 
-            return database_api.message_return(data,200)
+            return database_api.message_return(data[0],200)
 
         else: # Si No Existe
 
