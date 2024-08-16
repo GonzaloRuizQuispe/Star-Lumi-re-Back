834a4125e13df7335107deafc7b4d77919984ff4
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2024-08-16 09:25:25 -0300

    pepe 2

--- BACKEND/libs/database_c.py
+++ BACKEND/libs/database_c.py
@@ -1,5 +1,5 @@
 import os
-import pymysql
+import psycopg2
 from flask import make_response
 from dotenv import load_dotenv
 
@@ -12,8 +12,7 @@ class database_c():
     # Variables Recursivas
     conexion = None
     db = None
-    codigo = os.getenv("OTP")
-    puerto = int(os.getenv("DATABASE_PORT"))
+    DATABASE_URL = os.environ.get('POSTGRES_URL')
 
     # Retorno De Mensajes
     def message_return(self, text, status_code):
@@ -29,15 +28,9 @@ class database_c():
     # Conectarse a la DB
     def conectar_db(self):
         try:
-            self.conexion = pymysql.connect(
-            host=os.getenv("DATABASE_HOST"),
-            user=os.getenv("DATABASE_USER"),
-            passwd=os.getenv("DATABASE_PASSWORD"),
-            db=os.getenv("DATABASE_NAME"),
-            port=self.puerto,
-            autocommit=True,
-            ssl={ "rejecUnauthorized": False }
-        )
+            
+            self.conexion = psycopg2.connect(self.DATABASE_URL)
+
             self.db = self.conexion.cursor()
 
         except Exception as e:
@@ -100,35 +93,3 @@ class database_c():
 database_api = database_c()
 
 #from star_lumiere.star_lumire import api_star_lumiere
-
-
-""" # MOVER POR SEPARADO AGREGAR CATEGORIAS
-resp = api_star_lumiere.view_categories()
-
-database_api.conectar_db()
-
-for x in resp:
-    database_api.db.execute("INSERT INTO C_Service (id_c_plataform,name) VALUES ('1','{}')".format(x))
-
-database_api.desconectar_db()
-# UP
- """
-
-
-""" #MOVER POR SEPARADO AGREGAR ID SERVICIOS
-database_api.conectar_db()
-
-resp = api_star_lumiere.view_services()
-
-database_api.db.execute("SELECT * FROM C_Service")
-
-resp_1 = database_api.db.fetchall()
-
-for x in resp:
-    for y in resp_1:
-        if x['category'] == y[2]:
-            database_api.db.execute("INSERT INTO Service (id_original,id_c_service) VALUES ('{}','{}')".format(x['service'],y[0]))
-            pass
-        else:
-            pass
-#UP """
