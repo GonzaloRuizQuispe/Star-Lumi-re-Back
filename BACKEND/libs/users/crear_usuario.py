7a581f41008c1db13ad08ebc66aa27901aba7083
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2024-08-16 09:42:23 -0300

    create user

--- BACKEND/libs/users/crear_usuario.py
+++ BACKEND/libs/users/crear_usuario.py
@@ -1,19 +1,23 @@
 from libs.database_c import database_api
 from libs.tokens.gen_tokens import tokens_api
 
-def crear_usuario_api(username,password,email): # Crear Usuario En La DB
-        
-        resp_0 = database_api.control_db("SELECT * FROM Usuarios WHERE email='{}'".format(email)) #Consultar Si Existe El Correo
+def crear_usuario_api(username, password, email):  # Crear Usuario En La DB
 
-        if not resp_0: # Si La Lista Esta Vacia Se Accede
-            
-            token_header = tokens_api.gen_token_header() # Generar Token Header, Identificador Web
-            token_acceso = tokens_api.gen_token_acceso() # Generar Token A2F De Acceso
+    # Consulta parametrizada para evitar inyecciones SQL
+    resp_0 = database_api.control_db("SELECT * FROM Usuarios WHERE email = %s", (email,))  # Consultar Si Existe El Correo
 
-            resp_1 = database_api.control_db("INSERT INTO Usuarios (username,password,email,token_header,token_acceso) VALUES ('{}','{}','{}','{}','{}')".format(username,password,email,token_header,token_acceso)) #Se Guardan Los Datos
+    if not resp_0:  # Si La Lista Está Vacía, Se Accede
 
-            return database_api.message_return("user created",201)
+        token_header = tokens_api.gen_token_header()  # Generar Token Header, Identificador Web
+        token_acceso = tokens_api.gen_token_acceso()  # Generar Token A2F De Acceso
 
-        else: # Si La Lista No Esta Vacia
-            
-            return database_api.message_return("email exists",400)
+        # Consulta segura utilizando parámetros
+        resp_1 = database_api.control_db(
+            "INSERT INTO Usuarios (username, password, email, token_header, token_acceso) VALUES (%s, %s, %s, %s, %s)",
+            (username, password, email, token_header, token_acceso)
+        )  # Se Guardan Los Datos
+
+        return database_api.message_return("user created", 201)
+
+    else:  # Si La Lista No Está Vacía
+        return database_api.message_return("email exists", 400)
